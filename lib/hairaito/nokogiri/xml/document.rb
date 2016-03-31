7792778ab8c8e1280f5b06b4d6a14203ed91e920
Denis Mazilov <denis.mazilov@gmail.com>
2016-03-31 11:37:03 +0300

    Added word boundaries options processing.

--- lib/hairaito/nokogiri/xml/document.rb
+++ lib/hairaito/nokogiri/xml/document.rb
@@ -3,80 +3,86 @@ module Hairaito
     module XML
       module Document
 
-        def highlight(snippets, options = {})
-          highlighting_default_options(options)
+        # Highlights text snippets in document
+        #
+        # @param snippets [Array<String>] text variants to be highlighted
+        # @param options [Hash] custom highlighting options
+        # @return [Nokogiri::XML::Document] self document for chaining
+        def highlight(snippets, options)
+          highlighting_defaults(options)
+          snippet_parts_to_wrap = []
           snippets.each do |snippet|
-            highlighting_base.traverse_by_text(snippet).each do |snippet_container|
-              to_wrap = []
-              start_index = snippet_container.text().index(snippet)
-
-              start_node, start_inner_index = snippet_container.text_node_by_position(start_index)
+            highlighting_base.traverse_by_text(snippet, @hl_opts[:boundaries]) do |snippet_container, snippet_offset|
+              start_node, start_inner_index = snippet_container.text_node_by_position(snippet_offset.first)
               start_range = start_node.text_range_by_index(start_inner_index, snippet.length)
-              to_wrap << [start_node, start_range]
+              snippet_parts_to_wrap << {part: start_node, range: start_range, starting: true}
 
               # If start node contains only part of snippet
               if snippet.length > start_range.size
-                end_node, end_inner_index = snippet_container.text_node_by_position(start_index + snippet.length - 1)
+                end_node, end_inner_index = snippet_container.text_node_by_position(snippet_offset.last - 1)
                 end_range = end_node.text_range_by_index(end_inner_index)
-                to_wrap += snippet_container.text_nodes_between(start_node, end_node).map do |node|
-                  [node, 0..(node.text.length - 1)]
+                snippet_parts_to_wrap += snippet_container.text_nodes_between(start_node, end_node).map do |node|
+                  {part: node, range: 0..(node.text.length - 1)}
                 end
-                to_wrap << [end_node, end_range]
+                snippet_parts_to_wrap << {part: end_node, range: end_range}
               end
-
-              to_wrap.each do |node_data|
-                node_data.first.highlight_by_range(node_data.last)
-              end
-
-              snippet_container['class'] = "#{snippet_container['class']} #{@hl_opts[:snippet_container_class]}"
             end
           end
-          numerate_highlighted_snippets if @hl_opts[:numerate]
-          to_html
-        end
-
-        def highlight_snippet_part(text)
-          if @hl_opts[:snippet_part_wrapper].blank?
-            raise ArgumentError.new('Snippet part wrapper tag is not specified!')
+          snippet_parts_to_wrap.group_by{|part_data| part_data[:part]}.each do |part, parts_collection|
+            part.highlight_by_ranges(parts_collection.map{|p| p.except(:part)}, @hl_opts)
           end
-          wrapper = create_element("#{@hl_opts[:snippet_part_wrapper]}", class: "#{@hl_opts[:snippet_part_wrapper_class]}")
-          wrapper.content = text
-          wrapper
+          numerate_snippet_parts if @hl_opts[:numeration][:attr].present?
+          self
         end
 
         private
 
-        def highlighting_default_options(options)
+        def highlighting_defaults(options)
+          @hl_base = nil
           @hl_opts = {
-              base_selector: 'body',
-              base_content_wrapper: '',
-              base_content_wrapper_class: 'highlighting-base',
-              snippet_container_class: 'highlighted-snippet',
-              snippet_part_wrapper: 'span',
-              snippet_part_wrapper_class: 'highlighted-snippet-part',
-              numerate: true,
-              numeration_attr: 'data-snippet-id',
-              numeration_prefix: '',
-              numeration_suffix: '',
-          }.merge(options)
+              highlight_base: {
+                  selector: 'body',
+                  content_wrapper: '',
+                  content_wrapper_class: 'highlighting-base',
+              },
+              snippet: {
+                  part_tag: 'span',
+                  part_class: 'snippet-part',
+                  part_additional_classes: '',
+                  starting_part_class: 'snippet-start',
+              },
+              numeration: {
+                  attr: 'data-snippet-id',
+                  prefix: '',
+                  suffix: '',
+                  start_with: 0,
+              },
+              boundaries: {},
+          }.deep_merge(options).with_indifferent_access
         end
 
         def highlighting_base
-          base = at(@hl_opts[:base_selector])
+          return @hl_base if @hl_base.present?
+          base = at(@hl_opts[:highlight_base][:selector])
           raise ArgumentError.new('Document does not contain highlighting base element!') if base.blank?
-          if @hl_opts[:base_content_wrapper].present?
-            wrapper = create_element("#{@hl_opts[:base_content_wrapper]}", class: "#{@hl_opts[:base_content_wrapper_class]}")
+          if @hl_opts[:highlight_base][:content_wrapper].present?
+            wrapper = create_element("#{@hl_opts[:highlight_base][:content_wrapper]}",
+                                     class: "#{@hl_opts[:highlight_base][:content_wrapper_class]}")
             base.children.each{|child| child.parent = wrapper}
             wrapper.parent = base
-            return wrapper
+            @hl_base = wrapper
+          else
+            @hl_base = base
           end
-          base
+          @hl_base
         end
 
-        def numerate_highlighted_snippets
-          css(".#{@hl_opts[:snippet_container_class]}").each_with_index do |snippet_container, index|
-            snippet_container[@hl_opts[:numeration_attr]] =
-                "#{@hl_opts[:numeration_prefix]}#{index}#{@hl_opts[:numeration_prefix]}"
+        def numerate_snippet_parts
+          selector = @hl_opts[:snippet][:part_class].gsub(/\s+/, ' ').split(' ').map{|cl| ".#{cl}"}.join('')
+          index = @hl_opts[:numeration][:start_with] - 1
+          css(selector).each do |part|
+            index += 1 if part[:class].split(' ').include?(@hl_opts[:snippet][:starting_part_class])
+            part[@hl_opts[:numeration][:attr]] = "#{@hl_opts[:numeration][:prefix]}#{index}#{@hl_opts[:numeration][:suffix]}"
           end
         end
 
--- lib/hairaito/nokogiri/xml/node.rb
+++ lib/hairaito/nokogiri/xml/node.rb
@@ -3,6 +3,7 @@ module Hairaito
     module XML
       module Node
 
+        # @return [Nokogiri::XML::NodeSet] all text nodes, that has self as ancestor
         def text_nodes
           result_nodes = []
           traverse do |node|
@@ -12,6 +13,17 @@ module Hairaito
           ::Nokogiri::XML::NodeSet.new(document, result_nodes)
         end
 
+        # @return [Nokogiri::XML::Node] first text node within self node
+        def first_text_node
+          traverse do |node|
+            return node if node.text?
+          end
+          nil
+        end
+
+        # @param start_node [Nokogiri::XML::Node] left boundary
+        # @param end_node [Nokogiri::XML::Node] right boundary
+        # @return [Nokogiri::XML::NodeSet] all text nodes are located between specified boundaries
         def text_nodes_between(start_node, end_node)
           nodes = text_nodes
           indexes = [nodes.index(start_node), nodes.index(end_node)]
@@ -22,18 +34,83 @@ module Hairaito
           ::Nokogiri::XML::NodeSet.new(document, result_nodes)
         end
 
-        def traverse_by_text(text, exclude_ancestors = true)
-          excluded = []
-          result_nodes = []
-          traverse do |node|
-            next if node.is_a?(::Nokogiri::XML::Text)
-            next if node.in?(excluded)
-            if node.text.include?(text)
-              result_nodes << node
-              excluded += node.ancestors if exclude_ancestors
+        # @param base [Nokogiri::XML::Node] root element for search
+        # @return [Nokogiri::XML::Node, nil] previous text node within base node or nil if it doesn't exist
+        def previous_text(base = document)
+          first_text_node = text_nodes.first
+          base_text_nodes = base.text_nodes
+          if (index = base_text_nodes.index(first_text_node)).blank?
+            raise ArgumentError.new('Base must contain self node!')
+          end
+          return if index == 0
+          base_text_nodes[index - 1]
+        end
+
+        # @param base [Nokogiri::XML::Node] root element for search
+        # @return [Nokogiri::XML::Node, nil] next text node within base node or nil if it doesn't exist
+        def next_text(base = document)
+          first_text_node = text_nodes.last
+          base_text_nodes = base.text_nodes
+          if (index = base_text_nodes.index(first_text_node)).blank?
+            raise ArgumentError.new('Base must contain self node!')
+          end
+          return if index == base_text_nodes.count - 1
+          base_text_nodes[index + 1]
+        end
+
+        # Yields for each match of specified string in child nodes recursively
+        #
+        # @yieldparam node [Nokogiri::XML::Node] child node contains specified string
+        # @yieldparam offset [Array] child text inner offset
+        # @param string [String] text for matching
+        # @param options [Hash] @see #traverse_by_text_default_options
+        # @return [Nokogiri::XML::Node] self node for chaining
+        def traverse_by_text(string, options, &block)
+          traverse_by_text_defaults(options)
+          traverse do |current_node|
+            next if current_node.text?
+
+            offset_types = @tbt_opts[:whole_words_only] ? [:inner_word, :boundary_word] : [:simple]
+            inner_offsets, boundary_offsets = current_node.matched_offsets(string, offset_types, @tbt_opts)
+
+            # Check words bordered with current inline tag if current node has boundary words
+            # abc<span>def<span> or <span>def</span>ghi or abc<span>def</span>ghi
+            if current_node.name.in?(@tbt_opts[:inline_tags]) && self != current_node
+              if boundary_offsets.try(:first).try(:first) == 0
+                previous_node = current_node.previous_text(self)
+                boundary_offsets.shift if previous_node.try(:matched_offsets, :any, :ending_word, @tbt_opts).present?
+              end
+              if boundary_offsets.try(:last).try(:first) == 0
+                next_node = current_node.next_text(self)
+                boundary_offsets.pop if next_node.try(:matched_offsets, :any, :beginning_word, @tbt_opts).present?
+              end
+            end
+
+            offsets = (inner_offsets + (boundary_offsets || [])).sort_by{|offset| offset.first}
+            if offsets.any?
+              offsets.each {|offset| yield(current_node, offset)} if block_given?
+              if current_node != self
+                # Excludes processed offsets from all ancestors
+                current_node.ancestors.each do |ancestor|
+                  pos = ancestor.position_by_text_node(current_node.first_text_node)
+                  # Shifts all offsets according to ancestor inner position
+                  ancestor.exclude_offsets(offsets.map{|offset| [offset.first + pos, offset.last + pos]})
+                  # Reaches highlighting base
+                  break if ancestor == self
+                end
+              end
             end
           end
-          result_nodes
+          self
+        end
+
+        def position_by_text_node(text_node)
+          nodes = text_nodes
+          if (index = nodes.index(text_node)) < 0
+            raise ArgumentError.new('Self node must contain text_node!')
+          end
+          return 0 if index == 0
+          nodes[0..index - 1].map{|node| node.text}.join('').length
         end
 
         def text_node_by_position(in_text_position)
@@ -48,16 +125,75 @@ module Hairaito
           raise ArgumentError.new('Inner index is out of range!')
         end
 
-        def highlight_by_range(range)
-          prefix = range.first > 0 ? text[0..(range.first - 1)]: ''
-          suffix = range.last < text.length - 1 ? text[(range.last + 1)..(text.length - 1)]: ''
-          for_wrapping = text[range]
-          new_contents = "#{prefix}#{document.highlight_snippet_part(for_wrapping)}#{suffix}"
+        def highlight_by_ranges(ranges, options)
+          if options[:snippet][:part_tag].blank?
+            raise ArgumentError.new('Snippet part wrapper tag is not specified!')
+          end
+          parts = []
+          ranges = ranges.sort_by{|r| r[:range].first}
+          ranges.each_with_index do |range_data, index|
+            range = range_data[:range]
+            parts << (range.first > 0 ? text[0..(range.first - 1)]: '') if index == 0
+            snippet_class = range_data[:starting] ? "#{options[:snippet][:starting_part_class]}" : ''
+            wrapper = document.create_element("#{options[:snippet][:part_tag]}", class: "#{options[:snippet][:part_class]} #{snippet_class}")
+            wrapper.content = text[range]
+            parts << wrapper.to_s
+            parts << text[(range.last + 1)..(ranges[index + 1][:range].first - 1)] if index < ranges.count - 1
+            parts << (range.last < text.length - 1 ? text[(range.last + 1)..(text.length - 1)]: '') if index == ranges.count - 1
+          end
+          new_contents = parts.join('')
           replace(new_contents)
         end
 
         def text_range_by_index(index, demand_length = nil)
           demand_length.present? ? index..[text.length - 1, index + demand_length - 1].min : 0..index
+        end
+
+        def excluded_offsets
+          @excluded_offsets ||= []
+        end
+
+        def exclude_offsets(offsets)
+          @excluded_offsets ||= []
+          @excluded_offsets += offsets
+        end
+
+        def matched_offsets(string, types, options)
+          types = [types] unless types.is_a?(Array)
+          offsets = []
+          types.each do |type|
+            offsets << text.to_enum(:scan, build_regexp(string, type, options)).map do
+              offset = Regexp.last_match.offset(:text)
+              offset unless excluded_offsets.include?(offset)
+            end.compact || []
+          end
+          return *offsets
+        end
+
+        private
+
+        def traverse_by_text_defaults(options)
+          @tbt_opts = {
+              whole_words_only: true,
+              inline_tags: %w(a b i s u basefont big em font img label small span strike strong sub sup tt),
+              word_parts: '[а-яА-ЯёЁa-zA-Z\d]',
+          }.deep_merge(options).with_indifferent_access
+        end
+
+        def build_regexp(string, type = :simple, options)
+          string = '.+' if string == :any
+          case type.to_sym
+            when :simple
+              return /(?<text>#{string})/
+            when :inner_word
+              return /(?<!#{options[:word_parts]}|\A)(?<text>#{string})(?!#{options[:word_parts]}|\Z)/
+            when :beginning_word
+              return /\A(?<text>#{string})(?!#{options[:word_parts]})/
+            when :ending_word
+              return /(?<!#{options[:word_parts]})(?<text>#{string})\Z/
+            when :boundary_word
+              return /(\A(?<text>#{string})(?!#{options[:word_parts]}))|((?<!#{options[:word_parts]})(?<text>#{string})\Z)|(\A(?<text>#{string})\Z)/
+          end
         end
 
       end
